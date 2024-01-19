c564fea4f8e22c3d68dce8b9d8e8dcfa80eb8e8c
kameshdhana <kamesh.dhanasekar@jaam.co>
2024-01-19 23:50:38 +0530

    Nested dynamic routes

---
+++ nextjs-mosh/nextjs-latest-14/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReviewNumber = ({params}:{params:{productId:string,reviewId:string}}) => {
+  return (
+    <h1>Review {params.reviewId} for product {params.productId}</h1>
+  )
+}
+
+export default ReviewNumber
---
+++ nextjs-mosh/nextjs-latest-14/src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductReviews = () => {
+  return (
+    <h1>Product Reviews List</h1>
+  )
+}
+
+export default ProductReviews
