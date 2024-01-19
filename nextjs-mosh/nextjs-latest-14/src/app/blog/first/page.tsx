82492cc8e9738d788bcff3e11aa0efc3809ff449
kameshdhana <kamesh.dhanasekar@jaam.co>
2024-01-19 23:17:00 +0530

    Nested routes

---
+++ nextjs-mosh/nextjs-latest-14/src/app/blog/first/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogFirst = () => {
+  return (
+    <div>First Blog</div>
+  )
+}
+
+export default BlogFirst
---
+++ nextjs-mosh/nextjs-latest-14/src/app/blog/second/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogSecond = () => {
+  return (
+    <div>Second Blog</div>
+  )
+}
+
+export default BlogSecond
