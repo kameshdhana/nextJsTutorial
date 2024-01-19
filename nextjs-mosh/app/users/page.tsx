14dc65dd3f12ff7fae92efdf2f98e00f373a186c
kameshdhana <kamesh.dhanasekar@jaam.co>
2024-01-19 16:58:43 +0530

    Data fetching using server components

--- nextjs-mosh/app/users/page.tsx
+++ nextjs-mosh/app/users/page.tsx
@@ -2,12 +2,22 @@ import Link from 'next/link';
 import React from 'react'
 import ProductCard from '../components/ProductCard';
 
-const users = () => {
+interface User {
+  id: number;
+  name: string;
+}
+
+const users = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const users: User[] = await res.json();
   return (
     <div>
-        <h1>Users Page</h1>
-        <Link href={'/users/nestedroute'}>Navigate to nested Route</Link>
-        <ProductCard/>
+      <h1>Users List</h1>
+      <ul>
+        {(users && users.map((x) => { return (<li key={x.id}>{x.name}</li>); }))}
+      </ul>
+      <Link href={'/users/nestedroute'}>Navigate to nested Route</Link>
+      <ProductCard />
     </div>
   )
 }
