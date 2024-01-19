1008517d616546850fa6632410a2e0aa521a973a
kameshdhana <kamesh.dhanasekar@jaam.co>
2024-01-20 00:23:52 +0530

    Common and Basic not found error

---
+++ nextjs-mosh/nextjs-latest-14/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div>
+        <h2>Page Not Found</h2>
+        <p>Could Not Find Requested Resource</p>
+    </div>
+  )
+}
+
+export default NotFoundPage
--- nextjs-mosh/nextjs-latest-14/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ nextjs-mosh/nextjs-latest-14/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { notFound } from 'next/navigation';
+import ReviewNotFound from './not-found';
 
 const ReviewNumber = ({params}:{params:{productId:string,reviewId:string}}) => {
-  return (
-    <h1>Review {params.reviewId} for product {params.productId}</h1>
-  )
+  if(parseInt(params.reviewId)>10){
+    // return notFound();
+    return ReviewNotFound();
+  }
+  else{
+    return (
+      <h1>Review {params.reviewId} for product {params.productId}</h1>
+    )
+  }
 }
 
 export default ReviewNumber
